83a30dda217dcf0168c68c32c485ad4db9d111b3
Dranell <dranellwilson!@gmail.com>
2024-06-27 14:35:37 -0500

    Created a function to create a database in MySQL

---
+++ create_data_base.py
@@ -0,0 +1,2 @@
+create_data_base = "create database car_dealership"
+#on the left we are creating a value for python that is equal to what we are typing in mySQL
--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import mysql.connector
 from mysql.connector import Error
 
+#import your create_data_base.py
+import create_data_base as cdb
+
 #Create a function to authenticate your connection to MySQL
 def createServerConnection(host_name,user_name,user_password):
     connection = None
@@ -15,5 +18,20 @@ def createServerConnection(host_name,user_name,user_password):
     except Error as err:
         print(f"Error:{err}")
     return connection
+
+#create a function that will create a database
+def create_database(connection,query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        #cursor is created by the connection to mySQL. Think of the cursor as the blinking box ready to be used by the user. We are telling the cursor to
+        #execute the query.
+        print("Database created successfully!")
+    except Error as err:
+        print(f"Error: {err}")
+
 #call function to establish connection from main.py to MySQL. Host name will always be localhost. User name is always "root".
-createServerConnection("localhost","root","student")
+#make sure you set the calling of this function equal to 'connection'
+connection = createServerConnection("localhost","root","student")
+#call function to create Database (this will only be called once. After that we delete it.)
+create_database(connection,cdb.create_data_base )
